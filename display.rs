47e4838b35491ed36e3095847582db3f60d8401b
Amiran Malania <amalania@qarva.com>
2023-02-20 22:54:28 +0400

    wip

--- display.rs
+++ display.rs
@@ -21,6 +21,18 @@ impl fmt::Display for Point2D {
     }
 }
 
+#[derive(Debug)]
+struct Complex {
+    real_part: f64,
+    img_part: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real_part, self.img_part)
+    }
+}
+
 
 fn main() {
     let minmax = MinMax(0, 14);
@@ -32,11 +44,18 @@ fn main() {
     let big_range = MinMax(-300, 300);
     let small_range = MinMax(-3, 3);
 
-    println!("The big range is {bog} and the small is {small}", small = small_range, big = big_range);
+    println!("The big range is {big} and the small is {small}", small = small_range, big = big_range);
 
     let poit = Point2D {x: 3.3, y: 7.2};
 
     println!("Compare points:");
     println!("Display: {}", poit);
-    println!("Debug: {}", poit);
+    println!("Debug: {:?}", poit);
+
+    let i = Complex { real_part: 3.3, img_part: 7.2 };
+
+    println!("Complex number:");
+    println!("Display: {}", i);
+    println!("Debug: {:?}", i);
+
 }
