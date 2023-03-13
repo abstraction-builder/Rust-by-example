2a465c87a066fdeb7ff6779e9a587c48e62f02b2
Amiran Malania <amalania@qarva.com>
2023-03-13 10:34:13 +0400

    wip

--- display.rs
+++ display.rs
@@ -32,7 +32,7 @@ fn main() {
     let big_range = MinMax(-300, 300);
     let small_range = MinMax(-3, 3);
 
-    println!("The big range is {bog} and the small is {small}", small = small_range, big = big_range);
+    println!("The big range is {big} and the small is {small}", small = small_range, big = big_range);
 
     let poit = Point2D {x: 3.3, y: 7.2};
 
